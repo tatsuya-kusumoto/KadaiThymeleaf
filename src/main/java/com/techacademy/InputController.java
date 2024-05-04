45de174a3459be557b1c7cb94d218fb451aadc1a
tatsuya-kusumoto <tkusumot118217@gmail.com>
2024-05-04 23:01:17 +0900

    Lesson15 課題再提出

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -1,13 +1,16 @@
 package com.techacademy;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InputController {
 
     @GetMapping("/input")
-    public String getInput() {
+    public String getInput(@RequestParam(name = "previous",required = false)String val,Model model) {
+        model.addAttribute("val",val);
         return "input";
     }
 
